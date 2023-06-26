902050cb168f315d385e112fe6230171f823ad0d
Gurijala.Sripal <gurijala.sripal@ivycomptech.com>
2023-06-26 17:32:07 +0530

    first commit

---
+++ playcanvas/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1725123.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 130374796, 130374773, 130374783, 130374786, 130374792, 130374793, 130374775, 130374779, 130374787, 130374789, 130374804 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/130375290/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/130375291/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/130375289/1/ammo.js', 'preload' : true},
+];
